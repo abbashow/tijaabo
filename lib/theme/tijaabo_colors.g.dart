299208414a25afa9c76b9438fb3fd56b75628ef4
Parabeac-Nest <admin@parabeac.com>
2022-11-10 17:45:53 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/tijaabo_colors.g.dart
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TijaaboColors {
+
+static const Color pink = Color(0xfff240ae);
+
+static const Color yellow = Color(0xfffcdb14);
+
+static const Color green = Color(0xff01f9c5);
+
+static const Color gray3 = Color(0xff828282);
+
+static const Color gray1 = Color(0xff333333);
+
+static const Color gray4 = Color(0xffbdbdbd);
+
+static const Color gray6 = Color(0xfff2f2f2);
+}
